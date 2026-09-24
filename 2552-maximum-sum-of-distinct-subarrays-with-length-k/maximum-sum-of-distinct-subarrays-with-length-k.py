4fb150c69018edafe59a8fa733fa382c6cb24fee
Aaysha Mansuri <147688939+AayCode@users.noreply.github.com>
2026-09-25 00:51:06 +0530

    Time: 127 ms (37.10%) | Memory: 37.1 MB (71.64%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,25 +1,66 @@
 class Solution:
     def maximumSubarraySum(self, nums: list[int], k: int) -> int:
-        i = window_sum = max_sum = 0
-        seen = set()
+        # i = window_sum = max_sum = 0
+        # seen = set()
 
-        for j in range(len(nums)):
-            while nums[j] in seen:
-                window_sum -= nums[i]
-                seen.remove(nums[i])
-                i += 1
+        # for j in range(len(nums)):
+        #     while nums[j] in seen:
+        #         window_sum -= nums[i]
+        #         seen.remove(nums[i])
+        #         i += 1
 
-            window_sum += nums[j]
-            seen.add(nums[j])
+        #     window_sum += nums[j]
+        #     seen.add(nums[j])
      
-            if len(seen) == k:
-                max_sum = max(window_sum, max_sum)
-                window_sum -= nums[i]
-                seen.remove(nums[i])
-                i += 1
+        #     if len(seen) == k:
+        #         max_sum = max(window_sum, max_sum)
+        #         window_sum -= nums[i]
+        #         seen.remove(nums[i])
+        #         i += 1
                 
 
-        return max_sum
+        # return max_sum
+
+        i = 0
+        summ = maxx = dups = 0
+        seen = {}
+
+        for j in range(k):
+            summ += nums[j]
+            if nums[j] not in seen:
+                seen[nums[j]] = 0
+            seen[nums[j]] += 1
+
+            if seen[nums[j]] > 1:
+                dups += 1
+
+        if dups == 0:
+            maxx = max(summ, maxx)
+
+        for j in range(k, len(nums)):
+            summ += nums[j]
+            if nums[j] not in seen:
+                seen[nums[j]] = 0
+            seen[nums[j]] += 1
+
+            if seen[nums[j]] > 1:
+                dups += 1
+
+            summ -= nums[i]
+            if seen[nums[i]] > 1:
+                dups -= 1
+            seen[nums[i]] -= 1
+            i += 1
+
+            if dups == 0:
+                maxx = max(summ, maxx)
+
+        return maxx
+
+
+
+
+
 
 
         
