c5fcfe7c72f3385250587ff5f5c79ac282594b72
Aaysha Mansuri <147688939+AayCode@users.noreply.github.com>
2026-09-25 13:04:52 +0530

    Time: 63 ms (17.28%) | Memory: 36.3 MB (71.58%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -4,28 +4,42 @@ class Solution:
         seen = set() # 1 2 3
         n = len(nums)
 
-        if k >= n:
-            for j in range(n):
-                if nums[j] in seen:
-                    return True
-                seen.add(nums[j])
-        else:
-            for j in range(k+1):
-                if k == 0:
-                    return False
-                if nums[j] in seen:
-                    return True
-                seen.add(nums[j])
+        # if k >= n:
+        #     for j in range(n):
+        #         if nums[j] in seen:
+        #             return True
+        #         seen.add(nums[j])
+        # else:
+        #     for j in range(k+1):
+        #         if k == 0:
+        #             return False
+        #         if nums[j] in seen:
+        #             return True
+        #         seen.add(nums[j])
 
-        # 1 2 3 1 2 3
-        #       i
-        #           j
+        # # 1 2 3 1 2 3
+        # #       i
+        # #           j
 
-            for j in range(k+1, len(nums)):
+        #     for j in range(k+1, len(nums)):
+        #         seen.remove(nums[i])
+        #         i += 1
+        #         if nums[j] in seen:
+        #             return True
+        #         seen.add(nums[j])
+     
+        # return False
+
+        for j in range(n):
+            if k == 0:
+                return False
+            if nums[j] in seen:
+                return True
+            seen.add(nums[j])
+
+            if j - i >= k:
                 seen.remove(nums[i])
                 i += 1
-                if nums[j] in seen:
-                    return True
-                seen.add(nums[j])
-     
+
         return False
+
